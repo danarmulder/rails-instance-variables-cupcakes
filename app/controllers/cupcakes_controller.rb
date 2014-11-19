598e1acddd44b5345747e4c906047fac40d39837
Dana Mulder <danarmulder@gmail.com>
2014-11-19 13:24:02 -0800

    finish cupcake pages

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -12,10 +12,22 @@ class CupcakesController < ApplicationController
   end
 
   def maple_bacon
-
+    @sizes = [
+      'Mucho Grande',
+      'Grande',
+      'Medium',
+      'Small',
+      'Mini',
+    ]
   end
 
   def red_velvet
-
+    @ingredients = [
+      ['red', '4 drops'],
+      ['velvet', 'one sheet'],
+      ['egg', 'two'],
+      ['cocoa', 'one brick'],
+      ['cream cheese frosting', 'on the top'],
+    ]
   end
 end
